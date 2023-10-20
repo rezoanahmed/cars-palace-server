a484470e131b3a4ad55060c9e1492b8518f2e611
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-20 19:00:06 +0600

    vercel deployment issues solved by process.env.PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5090;
 require('dotenv').config()
 // middlewares
 app.use(cors())
